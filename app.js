1d46c7dea21472db4064b2883f9d3fff4224fb7a
Divyanshu Pandey <divyanshupandey1702@gmail.com>
2022-09-06 21:37:21 +0530

    Folder Structure Setup

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-// Import
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 
 const express = require("express");
+let body_parser = require("body-parser");
+const ejs = require("ejs");
 
 const app = express();
+app.use(
+  body_parser.urlencoded({
+    extended: true,
+  })
+);
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.render("main");
+});
+
+app.get("/login", function (req, res) {
+  res.render("auth");
+});
+
+app.get("/register", function (req, res) {
+  res.render("auth");
+});
+
+module.exports = app;
